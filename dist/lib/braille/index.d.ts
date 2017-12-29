e863a752d855682d1c856fbbd86d45840d97262e
Victor Ermolaev <victorermolaev@gmail.com>
2017-12-29 16:57:27 +0100

    Update dist

--- dist/lib/braille/index.d.ts
+++ dist/lib/braille/index.d.ts
@@ -1,10 +1,11 @@
 export declare type zero_one = 0 | 1;
 export declare type Braille = [[zero_one, zero_one], [zero_one, zero_one], [zero_one, zero_one], [zero_one, zero_one]];
 export declare function braille2string(b: Braille): string;
-export declare function image2braille(path: string, settings: {
+export interface BrailleArtSettings {
     white_cutoff: number;
     whitespace?: string | Braille;
     scale?: number;
     width?: number;
     height?: number;
-}): Promise<string[][]>;
+}
+export declare function image2braille(path: string, settings: BrailleArtSettings): Promise<string[][]>;
